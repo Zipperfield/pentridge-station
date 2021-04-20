ac03620bbc201aea39d05e71ab2b9f465a8a5e5d
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2021-04-19 20:07:55 -0400

    Add functionality that closes form when clicking outside of it

--- app/javascript/pages/book.js
+++ app/javascript/pages/book.js
@@ -1,3 +1,19 @@
+function hideOnClickOutside(parent, child) {
+    const outsideClickListener = event => {
+        if (!(event.defaultPrevented) && !child.contains(event.target) && isVisible(child)) {
+            removeClickListener();
+        }
+    }
+
+    const removeClickListener = () => {
+        toggleVisibility(parent);
+
+        document.removeEventListener('click', outsideClickListener);
+    }
+    document.addEventListener('click', outsideClickListener);
+}
+// || elem.getClientRects().length
+const isVisible = elem => !!elem && !!(elem.offsetWidth || elem.offsetHeight);
 
 function transferValue(eventValueName) {
     // Takes an event value name that is in the temporary form
@@ -11,37 +27,46 @@ function transferContent() {
     transferValue('event_date');
 }
 
-function toggleClassListVisibility(formClassList) {
-    if (formClassList.contains('flex')) {
-        formClassList.remove('flex');
-        formClassList.add('hidden');
-    } else if (formClassList.contains('hidden')) {
-        formClassList.remove('hidden');
-        formClassList.add('flex');
+function toggleVisibility(element) {
+    console.log("toggleVisibility")
+    if (element.classList.contains('flex')) {
+        element.classList.remove('flex');
+        element.classList.add('hidden');
+    } else if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+        element.classList.add('flex');
     } else {
-        console.log("Error: Form class lists are broken")
+        console.log("Error: Element neither hidden or flex")
     }
 }
 
 
 function toggleFormVisibility() {
-    tempFormClassList = document.getElementById('temp_event_form').classList;
-    formClassList = document.getElementById('event_form').classList;
-    toggleClassListVisibility(tempFormClassList);
+    // tempFormClassList = document.getElementById('temp_event_form_container').classList;
+    formClassList = document.getElementById('event_form_container').classList;
+    // toggleClassListVisibility(tempFormClassList);
     toggleClassListVisibility(formClassList);
 }
 
 document.addEventListener('turbolinks:load', () => {
     tempSubmit = document.getElementById("temp_submit");
+    eventFormScreen = document.getElementById('event_form_screen');
+    eventFormContainer = document.getElementById('event_form_container');
 
     tempSubmit.addEventListener("click", (event) => {
         event.preventDefault();
+        console.log(event);
 
         //   validate inputs
         // transfer content
         // toggle visibility
 
         transferContent();
-        toggleFormVisibility()
+        toggleVisibility(eventFormScreen);
+
+        hideOnClickOutside(eventFormScreen, eventFormContainer);
+        // formClassList = document.getElementById('event_form_container');
+        // formClassList.style.display = 
     });
+
 });
