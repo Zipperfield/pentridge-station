201607ac83cec7ac4e461eb02ec0c635f79d1520
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2021-04-20 10:56:36 -0400

    Update event form to pull from enum options

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,14 @@
 class Event < ApplicationRecord
   belongs_to :contact
-  enum event_type: %w[Wedding Party Music Meeting]
-  enum num_attendees: ['1-15', '16-30', '31-49', '50+']
+  enum event_type: { Wedding: 0, Party: 1, Music: 2, Meeting: 3 }
+  enum num_attendees: { '1-14' => 0, '15-29' => 1, '30-49' => 2, '50+' => 4 }
   accepts_nested_attributes_for :contact
+
+  def event_type=(val)
+    write_attribute :event_type, val.to_i
+  end
+
+  def num_attendees=(val)
+    write_attribute :num_attendees, val.to_i
+  end
 end
