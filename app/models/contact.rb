7610174cc89ce72f62ff7ea2c1d6d425fe832246
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2021-04-27 12:22:30 -0400

    Add Contact and Message model validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
-    enum entry_process: ['Newsletter', 'Message', 'Event']
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  enum entry_process: %w[Newsletter Message Event]
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :first_name, :email, presence: true
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,23 +2,17 @@ class Message < ActiveRecord::Base
   belongs_to :contact
   accepts_nested_attributes_for :contact
 
-
   # -- Relationships -----------------------------------------------------------
-
 
   # -- Callbacks ---------------------------------------------------------------
 
-
   # -- Validations -------------------------------------------------------------
-
+  validates :body, presence: true
+  validates_associated :contact
 
   # -- Scopes ------------------------------------------------------------------
 
-
   # -- Class Methods -----------------------------------------------------------
 
-
   # -- Instance Methods --------------------------------------------------------
-
-
 end
