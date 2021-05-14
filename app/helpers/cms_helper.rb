22526c8b4a79b488ba75bce5cd87e8876d2a2a8b
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2021-05-14 11:25:54 -0400

    Add nil guard for cms

--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -1,10 +1,14 @@
 module CmsHelper
   def get_photo_url(name)
+    return '' if @cms.nil?
+
     photo = cms_fragment_render(name, @cms)
     photo.nil? ? '' : photo
   end
 
   def get_text_content(name, backup)
+    return '' if @cms.nil?
+
     content = cms_fragment_render(name, @cms)
     content.empty? ? backup : content
   end
