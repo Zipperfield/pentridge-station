751cb2c1c23919d2869744a249b67f08654a7658
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2021-05-15 14:17:18 -0400

    Prep press section for carousel

---
+++ app/javascript/pages/home.js
@@ -0,0 +1,17 @@
+document.addEventListener('turbolinks:load', () => {
+    if (document.getElementById('header').getAttribute('page') != 'home') {
+        return
+    }
+
+    // Before I start this I Should research prebuilt solutions
+
+    // if small screen
+    //  toggle first article 
+
+    // loop through each article
+    //      toggle hidden and flex 
+    //      wait 
+    //      toggle again
+
+    // if large screen return to normal?
+});
