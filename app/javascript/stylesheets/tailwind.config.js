965acf8a5bb342682d6f30a3f7f78a7a92879def
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2021-05-29 18:19:17 -0400

    Change Tailwind blue colors to custom colors

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,8 +1,26 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: {
+        200: '#20f0ff',
+        300: '#0CC5D2',
+        400: '#00B7C4',
+        500: '#005960',
+        600: '#001e20',
+      },
+      green: colors.green,
+    },
   },
   variants: {
     extend: {},
