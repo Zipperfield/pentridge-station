83f711f495c3741d9f052d58f2c4fc3fd5c27c43
Julian DeGroot-Lutzner <jdegrootlutzner@gmail.com>
2021-07-14 12:13:11 -0400

    Reroute extraneous routes to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :schedules
-  end
-
-  namespace :admin do
     resources :events
     resources :partners
     resources :messages
@@ -27,7 +24,7 @@ Rails.application.routes.draw do
   comfy_route :cms_admin, path: '/admin'
   # Ensure that this route is defined last
 
-  # I dont think I want this >
+  get '*path', to: 'pages#home'
   comfy_route :cms, path: '/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
