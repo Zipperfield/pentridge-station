b0d22302fa29b726a8674aa026accc0670334abb
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2022-05-28 17:43:43 -0400

    Add basic HTTP auth to staging enviornment
    If a enviornment password is present, run basic auth. This is to prevent the staging enviornment from showing up in search engines and from users accidentally viewing work in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+                                password: ENV["HTTP_BASIC_PASSWORD"],
+                                if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
 end
